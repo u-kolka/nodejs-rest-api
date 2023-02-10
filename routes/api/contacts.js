e4048fb6dac33ff98c2163ae76cab78355f387b6
Nick Zheliba <nikolaj.jeliba@gmail.com>
2023-02-10 23:16:22 +0200

    add code

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const { tryCatchWrapper, validateBody } = require("../../helpers/error-func");
-const {
-    contactSchema,
-    updateContactSchema,
-} = require("../../validation/contacts-validation");
+const { contactSchema, updateContactSchema } = require("../../validation/contacts-validation");
 const {
     getContacts,
     getContact,
@@ -17,20 +14,9 @@ const routeContacts = express.Router();
 
 routeContacts.get("/", tryCatchWrapper(getContacts));
 routeContacts.get("/:contactId", tryCatchWrapper(getContact));
-routeContacts.post(
-    "/",
-    validateBody(contactSchema),
-    tryCatchWrapper(createContact)
-);
+routeContacts.post("/", validateBody(contactSchema), tryCatchWrapper(createContact));
 routeContacts.delete("/:contactId", tryCatchWrapper(deleteContact));
-routeContacts.put(
-    "/:contactId",
-    validateBody(updateContactSchema),
-    tryCatchWrapper(updateContact)
-);
-routeContacts.patch(
-    "/:contactId/favorite",
-    tryCatchWrapper(updateStatusContact)
-);
+routeContacts.put("/:contactId", validateBody(updateContactSchema), tryCatchWrapper(updateContact));
+routeContacts.patch("/:contactId/favorite", tryCatchWrapper(updateStatusContact));
 
 module.exports = routeContacts;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const { tryCatchWrapper, validateBody } = require("../../helpers/error-func");
-const { signupUser, loginUser, logoutUser, currentUser, updateUser, uploadImage } = require("../../controllers/users-controller");
-const { auth, upload } = require("../../helpers/middlewares")
-const {userSchema} = require("../../validation/user-validation")
+const { signupUser, loginUser, logoutUser, currentUser, updateUser, uploadImage, verifyEmail, repeatSendVerifyEmail,
+} = require("../../controllers/users-controller");
+const { auth, upload } = require("../../helpers/middlewares");
+const { userSchema } = require("../../validation/user-validation");
 
 const routeUsers = express.Router();
 
 routeUsers.post("/signup", validateBody(userSchema), tryCatchWrapper(signupUser));
-routeUsers.post("/login", validateBody(userSchema), tryCatchWrapper(loginUser)); 
+routeUsers.post("/login", validateBody(userSchema), tryCatchWrapper(loginUser));
 routeUsers.get("/logout", tryCatchWrapper(auth), tryCatchWrapper(logoutUser));
 routeUsers.get("/current", tryCatchWrapper(auth), tryCatchWrapper(currentUser));
-routeUsers.patch("/", tryCatchWrapper(auth), tryCatchWrapper(updateUser)); 
+routeUsers.patch("/", tryCatchWrapper(auth), tryCatchWrapper(updateUser));
 routeUsers.patch("/avatars", tryCatchWrapper(upload.single("avatar")), tryCatchWrapper(auth), tryCatchWrapper(uploadImage));
+routeUsers.get("/verify/:verificationToken", tryCatchWrapper(verifyEmail));
+routeUsers.post("/verify", tryCatchWrapper(repeatSendVerifyEmail));
 
 module.exports = routeUsers;
